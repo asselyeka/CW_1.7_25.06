ae703d0c0f324979338288e0c57dc7d316b025e5
Asselya Moldasheva <a.moldasheva@gmail.com>
2019-06-27 16:49:54 +0600

    Добавьте файлы проекта.

---
+++ CW_1.7_25.06/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+ 7.	В трехзначном числе x зачеркнули его вторую цифру. 
+ Когда к образованному при этом двузначному числу справа 
+ приписали вторую цифру числа x, то получилось число 456. 
+ Найти число x.
+     */
+namespace CW_1._7_25._06
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("В трехзначном числе x зачеркнули его вторую цифру.");
+            Console.WriteLine("Когда к образованному при этом двузначному числу справа");
+            Console.WriteLine("приписали вторую цифру числа x, то получилось число 456.");
+            var num = 456;
+            var initNum = 100 * (num / 100) + (num / 10) % 10 + 10 * (num % 10);
+
+            Console.Write("x=");
+            Console.WriteLine(initNum);
+            Console.ReadKey();
+        }
+    }
+}
